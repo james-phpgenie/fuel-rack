bb44de4f43ad04349bc8e377165266e723bdffbe
James Pudney <james@phpgenie.co.uk>
2012-08-24 17:20:31 +0100

    Started adding some functions.

--- classes/rack.php
+++ classes/rack.php
@@ -13,5 +13,37 @@ namespace Rack;
  
 class Rack
 {
+	/**
+	 * Constructor for the Rack package.
+	 * Loads the config settings and set up the object.
+	 **/
+	public function __construct()
+	{
+	}
 	
+	/**
+	 * Authenticates using the user credentials.
+	 * 
+	 * @param string auth_user The username of the user.
+	 * @param string auth_key	The authorisation key.
+	 * 
+	 * @return string auth_token Returns the authorisation token.
+	 **/
+	public function authenticate()
+	{
+	}
+	
+	/**
+	 * Returns a list of containers.  The maximum of 10,000 container names will be returned.
+	 * 
+	 * @param int $limit Limits the number of containers returned.
+	 * @param string $marker Returns object names greater in value than the specified marker. Only strings using UTF-8 encoding are valid.
+	 * @param string $format Specify either json or xml as the format for the returned values.  If left blank an array of strings will be returned.
+	 *
+	 * @return void
+	 * @author James Pudney
+	 **/
+	public function get_containers($limit = null, $marker = '', $format = '')
+	{
+	}
 }
